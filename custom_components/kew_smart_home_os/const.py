8b4700e98f443f4ddfc781a7ecb342a09188354a
danielwiedenau-max <danielwiedenau@googlemail.com>
2026-08-05 22:38:27 +0200

    Delete custom_components/kew_smart_home_os directory

--- custom_components/kew_smart_home_os/const.py
+++
@@ -1,7 +0,0 @@
-"""Constants for KEW Smart Home OS."""
-
-DOMAIN = "kew_smart_home_os"
-SERVICE_INSTALL = "install_assets"
-SERVICE_REFRESH = "refresh_assets"
-CONF_OVERWRITE = "overwrite_existing"
-DEFAULT_OVERWRITE = False
